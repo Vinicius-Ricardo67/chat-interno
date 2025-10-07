a71af16bfc6b8e9ee82077eb7cc258c9539c718f
Vinicius-ricardo <vinicius.ricardo@xpert.com.br>
2025-10-07 14:07:17 -0300

    Atualizando o login.js para ele funcionar com a nova tela de login

--- frontend/login.js
+++ frontend/login.js
@@ -1,7 +1,44 @@
-document.getElementById('loginBtn').addEventListener('click', () => {
-    const username = document.getElementById('username').value.trim();
-    if (!username) return alert("Digite seu nome!");
+const { catchError } = require("rxjs");
 
-    localStorage.setItem('username', username);
-    window.location.href = "index.html";
+const cadastroBtn = document.getElementById('cadastroBtn');
+const nomeInput = document.getElementById('nome');
+const emailInput = document.getElementById('email');
+const senhaInput = document.getElementById('senha');
+const imagemInput = document.getElementById('imagemPerfil');
+const irParaLogin = document.getElementById('irParaLogin');
+
+cadastroBtn.addEventListener('click', async () => {
+    const nome = nomeInput.value.trim();
+    const email = emailInput.value.trim();
+    const senha = senhaInput.value.trim();
+    const arquivo = imagemInput.files[0];
+
+    if (!nome || !email || !senha) return alert('Preencha todos os campos obrigatórios!');
+
+    const formData = new FormData();
+    formData.append('nome', nome);
+    formData.append('email', email);
+    formData.append('senha', senha);
+    if(arquivo) {
+        formData.append('imagem', arquivo);
+    }
+
+    try {
+        const res = await fetch("http://localhost:3000/api/cadastro", {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!res.ok) throw new Error('Erro ao cadastrar');
+
+        const data = await res.json();
+        alert('Cadastro realizado com sucesso');
+        window.href = 'index.html';
+    } catch (err) {
+        alert('Erro: ' + err.message);
+    }
+});
+
+irParaLogin.addEventListener('click', () => {
+    window.location.href = 'login.html';
 });
